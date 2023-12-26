93b3a4171683f0a8c3ff5cdef0c856470e715db1
Bartosz Kunat <bartosz.kunat@grandparade.co.uk>
2023-12-26 13:34:28 +0100

    Drop deployment target to macOS 13

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Main",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v13)],
     products: [
         .singleTargetLibrary("SnippetConverterCore"),
         .executable(name: "snippet-converter", targets: ["SnippetConverterCLI"])
